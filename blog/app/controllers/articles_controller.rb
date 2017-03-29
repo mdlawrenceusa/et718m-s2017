3b7d4d936d6fbcdc4cac48bfee9b4ca2cb5cd6d2
mdlawrenceusa <mdlawrence@me.com>
2017-03-29 10:21:15 -0400

    completed 5

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -3,6 +3,9 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def show
     @article = Article.find(params[:id])
@@ -11,6 +14,25 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 
   def create
     @article = Article.new(article_params)
@@ -26,5 +48,5 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :text)
   end
-  
+
 end
